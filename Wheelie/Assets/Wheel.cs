592d8491ee3235953702b07a6b8e95bdbbceeb22
3dquake <eetu.arvilommi@hotmail.com>
2015-08-31 22:10:10 +0300

    Add project files

---
+++ Wheelie/Assets/Wheel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
+public class Wheel : MonoBehaviour {
+
+    public new Rigidbody2D rigidbody
+    {
+        get
+        {
+            if (rbody == null)
+                rbody = GetComponent<Rigidbody2D>();
+
+            return rbody;
+        }
+    }
+    Rigidbody2D rbody;
+
+    public new CircleCollider2D collider
+    {
+        get
+        {
+            if (coll == null)
+                coll = GetComponent<CircleCollider2D>();
+
+            return coll;
+        }
+    }
+    CircleCollider2D coll;
+    
+}
